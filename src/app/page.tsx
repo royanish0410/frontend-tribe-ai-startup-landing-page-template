693c5e20c8f0cf6ea04b42aab1bf6e0485657d7c
royanish123 <anish.anandroy@gmail.com>
2025-04-03 00:40:13 +0530

    imported features section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Features } from "@/sections/Features";
 import { Header } from "@/sections/Header";
 import { Hero } from "@/sections/Hero";
 import { LogoTicker } from "@/sections/LogoTicker";
@@ -7,6 +8,7 @@ export default function Home() {
       <Header />
       <Hero />
       <LogoTicker />
+      <Features />
     </>
   );
 }
