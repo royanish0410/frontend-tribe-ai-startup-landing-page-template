0e6435eeb7ea573fd5facfedbe6101090c51b805
royanish123 <anish.anandroy@gmail.com>
2025-04-02 10:28:11 +0530

    Added button

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,5 +1,6 @@
 import LogoIcon from '@/assets/logo.svg';
-import MenuIcon from '@/assets/icon-menu.svg'
+import MenuIcon from '@/assets/icon-menu.svg';
+import { Button } from '@/components/Button';
 export const Header = () => {
   return( 
     <header className="py-4 border-b border-white/15 md:border-none">
@@ -19,14 +20,7 @@ export const Header = () => {
             </nav>
           </div>
           <div className="flex gap-4 items-center">
-            <button className="relative py-2 px-3 rounded-lg font-medium text-sm bg-gradient-to-b from-[#190d2e] to-[#4a208a] shadow-[0px_0px_12px_#8c45ff]">
-              <div className="absolute inset-0">
-                <div className="rounded-lg border border-white/20 absolute inset-0 [mask-image:linear-gradient(to_bottom,black,transparent)]"></div>
-                <div className="rounded-lg border absolute inset-0 border-white/40 [mask-image:linear-gradient(to_top,black,transparent)]"></div>
-                <div className="absolute inset-0 shadow-[0_0_10px_rgb(140,69,255,.7)_inset] rounded-lg"></div>
-              </div>
-              <span>Join Waitlist</span>
-            </button>
+            <Button>Join Waitlist</Button>
             <MenuIcon className="md:hidden"/>
           </div>
         </div>
