7c199ac509fd4f7c4f88dc9172d5942985c889f7
royanish123 <anish.anandroy@gmail.com>
2025-04-02 10:29:36 +0530

    Intialization of hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,33 @@
+import { Button } from "@/components/Button";
+import starsBg from "@/assets/stars.png"
+ 
 export const Hero = () => {
-  return <section>Hero</section>;
+  return(
+    <section
+      className="h-[492px] flex items-center"
+      style={{
+        backgroundImage: `url(${starsBg.src})`, 
+      }}
+    >
+      <div className="absolute h-64 w-64 bg-purple-500 rounded-full border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[radial-gradient(50%_50%_at_16.8%_18.3%,white,rgb(184,148,255)_37.7%,rgb(24,0,66))] shadow-[-20px_-20px_50px_rgb(255,255,255,.5),-20px_-20px_80px_rgb(255,255,255,.1),0_0_50px_rgb(140,69,255)]"></div>
+      <div className="absolute h-[344px] w-[344px] border opacity-20 rounded-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="absolute h-2 w-2 left-0 bg-white rounded-full top-1/2 -translate-x-1/2 -translate-y-1/2"></div>
+        <div className="absolute h-2 w-2 left-1/2 bg-white rounded-full top-0 -translate-x-1/2 -translate-y-1/2"></div>
+        <div className="absolute h-5 w-5 left-full border border-white rounded-full top-1/2 -translate-x-1/2 -translate-y-1/2 inline-flex items-center justify-center">
+          <div className="h-2 w-2 bg-white rounded-full"></div>
+        </div>
+      </div>
+      <div className="container relative">
+        <h1 className="text-8xl font-semibold tracking-tighter bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138,.5))] text-transparent bg-clip-text text-center">
+          AI SEO
+        </h1>
+        <p className="text-lg text-white/70 mt-5 text-center">
+          Elevate your site's visibility efforlessly with AI, where smart technology meets user-friendly SEO tools.
+        </p>
+        <div className="flex justify-center mt-5">
+          <Button>Join Waitlist</Button>
+        </div>
+      </div>
+    </section>
+  );
 };
